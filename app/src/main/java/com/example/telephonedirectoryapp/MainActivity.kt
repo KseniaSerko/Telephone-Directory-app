7d3273ff465020b7ab149cbeebc28fbe38dd2980
KseniaSerko <kseniaserko@gmail.com>
2024-04-21 00:28:37 +0300

    Reading Data

--- app/src/main/java/com/example/telephonedirectoryapp/MainActivity.kt
+++ app/src/main/java/com/example/telephonedirectoryapp/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.telephonedirectoryapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.telephonedirectoryapp.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var databaseReference: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchButton.setOnClickListener {
+            val searchContactNumber: String = binding.searchPhone.text.toString()
+            if (searchContactNumber.isNotEmpty()) {
+                readData(searchContactNumber)
+            } else {
+                Toast.makeText(this, "Please, enter the contact phone number", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
+
+    private fun readData(contactNumber: String) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("Contact Information")
+        databaseReference.child(contactNumber).get().addOnSuccessListener {
+            if (it.exists()) {
+                val contactName = it.child("name").value
+                val contactSurname = it.child("surname").value
+                val contactCompany = it.child("companyName").value
+                Toast.makeText(this, "Found according to your request", Toast.LENGTH_SHORT).show()
+                binding.searchPhone.text.clear()
+                binding.readContactName.text = contactName.toString()
+                binding.readContactSurname.text = contactSurname.toString()
+                binding.readContactCompany.text = contactCompany.toString()
+            } else {
+                Toast.makeText(this, "Can't find any matching number", Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
 }
